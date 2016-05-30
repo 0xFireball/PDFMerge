76dc6a38ddec4120f98f8aa1a87bd4b4c7b3b211
0xFireball <0xFireball@outlook.com>
2016-05-30 12:49:09 -0700

    add a basic application

---
+++ PDFMerge/PDFMerge/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace PDFMerge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string file1;
+        private string file2;
+        private string outputFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile1_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnFile2_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
