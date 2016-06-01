317965f3b61d8bba1ebe027d22b0b53c4d87bc25
0xFireball <0xFireball@outlook.com>
2016-05-31 17:19:30 -0700

    add mac project and icons

---
+++ PDFMerge/PDFMerge.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace PDFMerge.Mac
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
